ca2bb3e3d35d2fc85550003009dc8f8261ea8bcc
Colby Schulz <colbyschulz@Colbys-MBP.lan>
2022-08-18 15:57:50 -0400

    include table styles file

---
+++ src/components/table/table.css.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import colors from '../../colors';
+
+export const Table = styled.table`
+  border-collapse: 'collapse';
+  font-size: 13px;
+  font-weight: 500;
+`;
+
+export const THead = styled.thead``;
+
+export const Tr = styled.tr``;
+
+export const Th = styled.th`
+  padding: 12px 5px;
+  position: sticky;
+  top: 0;
+  background-color: ${colors.tan};
+`;
+
+export const Tbody = styled.tbody``;
